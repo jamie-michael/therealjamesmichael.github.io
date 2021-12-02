d29bee0312de1e964279bd501a8a8aad95d7be27
Jamie <internal@jamesmichael.uk>
2021-12-02 18:44:03 +0000

    added function that populates the CV links so that i only have to change it once if the link ever changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,44 @@
 const contactForm = document.querySelector('#contact');
 const emailOne = document.querySelector('.email-one');
+const emailMenu = document.querySelector('.email-menu');
 
 
+
+const populateCVLinks = () => {
+    const menu = document.querySelector('.cv-menu');
+    const one = document.querySelector('.cv-one');
+    const footer = document.querySelector('.cv-footer');
+
+    const arr = [menu, one, footer]; 
+    
+    for (const i of arr) {
+        i.href = 'https://drive.google.com/file/d/1zC2oECccdC_Kx7mmNLsPoswpTWemvqkE/view?usp=sharing';
+    }
+    
+    
+}
+
+
+
+populateCVLinks();
+
+
+
+
+
+// scroll to contact form from menu
+emailMenu.addEventListener('click', () => {
+    const cancelSidebarBtn = document.querySelector('.close');
+    
+    // close menu
+    cancelSidebarBtn.click();
+    setTimeout( () => {
+        contactForm.scrollIntoView({behavior: 'smooth'})
+    },500);
+    
+    
+});
+// scroll to contact box from ONE banner
 emailOne.addEventListener('click', () => {
     console.log('click');
     

057d4ea293a29d45a9cc62762754441480b72dd5
Jamie <internal@jamesmichael.uk>
2022-04-04 21:02:27 +0100

    changed ONE section and CONTACT section

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,16 +6,17 @@ const populateCVLinks = () => {
 		// CV links
 		if (e.classList.contains('cv-link')) {
 			e.href =
-				'https://drive.google.com/file/d/1a7jjoGlXV6kz1vg9djFi49S8d7zdIP28/view?usp=sharing';
+				'https://drive.google.com/file/d/1jwUvf38vT_23ET6oLa_Ruyq5HX-hQENU/view?usp=sharing';
 		}
 		// github links
 		if (e.classList.contains('github-link')) {
 			e.href = 'https://github.com/therealjamesmichael';
 		}
+		// Email links
 		if (e.classList.contains('email-link')) {
 			e.addEventListener('click', () => {
 				if (e.classList.contains('email-menu')) {
-					// different script for email link that is in the menu
+					// different script for email link which is located in the side menu
 					const cancelSidebarBtn = document.querySelector('.close');
 					cancelSidebarBtn.click();
 					setTimeout(() => {
